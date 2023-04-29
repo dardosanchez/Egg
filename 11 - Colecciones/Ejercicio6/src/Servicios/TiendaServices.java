9690d40697a409999655e58bd722946919224791
dardosanchez <dardosanchez2000@gmail.com>
2023-04-28 22:17:50 -0300

    Commit Final

--- 11 - Colecciones/Ejercicio6/src/Servicios/TiendaServices.java
+++ 11 - Colecciones/Ejercicio6/src/Servicios/TiendaServices.java
@@ -13,9 +13,10 @@ public class TiendaServices {
 
         char respuesta;
         do {
+        //    entrada.nextLine(); // LIMPIA EL BUFFER
             System.out.print("- Nombre del producto: ");
             String nombre = entrada.nextLine();
-
+            
             System.out.print("- Precio: $");
             Double precio = entrada.nextDouble();
 
@@ -23,53 +24,78 @@ public class TiendaServices {
             
             entrada.nextLine(); // LIMPIA EL BUFFER
             
-            System.out.print("Desea ingresar otro producto? (S/N)");
+            System.out.print("Desea ingresar otro producto? (S/N) ");
             respuesta = entrada.nextLine().charAt(0);
             respuesta = Character.toUpperCase(respuesta);
+            System.out.println("");
             
         } while (respuesta != 'N');
 
     }
-    
-    public void modificarPrecio (HashMap<String, Double> mapTienda){
-        
-        System.out.print("Que producto desea modificarle el precio? ");
-        String nombre = entrada.nextLine();
-        
-        if(mapTienda.containsKey(nombre)){
-           System.out.print("Valor a modificar: ");
-           Double valor = entrada.nextDouble();
-           
-           mapTienda.put(nombre,valor);
-           
+
+    public void modificarPrecio(HashMap<String, Double> mapTienda) {
+
+        if (mapTienda.isEmpty()) {
+            System.out.println("LA LISTA SE ENCUENTRA VACIA. AGREGUE PRODUCTOS");
         } else {
-            System.out.println("El producto no se encontro");
-        }
-    
-    }
-    
-    public void mostrarProductos (HashMap<String, Double> mapTienda){
-        
-        for (Map.Entry<String,Double> aux : mapTienda.entrySet()) {
-            String key = aux.getKey();
-            Double valor = aux.getValue();
             
-            System.out.println("Producto: " + key + " ->  Valor: " + valor);
+            System.out.print("Que producto desea modificarle el precio? ");
+            String nombre = entrada.nextLine();
+
+            if (mapTienda.containsKey(nombre)) {
+                System.out.print("Valor a modificar: ");
+                Double valor = entrada.nextDouble();
+
+                mapTienda.put(nombre, valor);
+                System.out.println("Precio modificado correctamente.");
+            } else {
+                System.out.println("El producto no se encontro");
+            }
+        }
+
+    }
+
+    public void mostrarProductos(HashMap<String, Double> mapTienda) {
+
+        if (mapTienda.isEmpty()) {
+            System.out.println("LA LISTA SE ENCUENTRA VACIA. AGREGUE PRODUCTOS");
+        } else {
+
+            for (Map.Entry<String, Double> aux : mapTienda.entrySet()) {
+                String key = aux.getKey();
+                Double valor = aux.getValue();
+
+                System.out.println("Producto: " + key + " ->  Valor: " + valor);
+            }
+        }
+
+    }
+
+    public void eliminarProducto(HashMap<String, Double> mapTienda) {
+
+        if (mapTienda.isEmpty()) {
+            System.out.println("LA LISTA SE ENCUENTRA VACIA. AGREGUE PRODUCTOS");
+        } else {
+           
+            System.out.print("Ingrese le producto a eliminar: ");
+            String producto = entrada.nextLine();
+
+            mapTienda.remove(producto);
+            System.out.println("Producto eliminado correctamente.");
+            System.out.println("");
+            
+        }
+
+    }
+
+    public int validarOpcion(int numero) {
+
+        while (numero != 1 && numero != 2 && numero != 3 && numero != 4 && numero != 5) {
+            System.out.println("Opcion ingresada incorrecta. Vuelva a ingresar.");
+            System.out.print("Ingrese una opcion: ");
+            numero = entrada.nextInt();
         }
         
+        return numero;
     }
-    
-    public void eliminarProducto (HashMap<String, Double> mapTienda){
-        
-        entrada.nextLine(); // LIMPIA EL BUFFER
-        System.out.print("Ingrese le producto a eliminar: ");
-        String producto = entrada.nextLine();
-        
-        
-        mapTienda.remove(producto);
-        
-        System.out.println("");
-        
-    }
-    
 }
--- 11 - Colecciones/Ejercicio6/src/tiendaAPP/NewMain.java
+++ 11 - Colecciones/Ejercicio6/src/tiendaAPP/NewMain.java
@@ -1,26 +1,58 @@
-
 package tiendaAPP;
 
 import Servicios.TiendaServices;
 import java.util.HashMap;
-
+import java.util.Scanner;
 
 public class NewMain {
 
-    
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
         HashMap<String, Double> map = new HashMap<>();
-        
+
         TiendaServices ts = new TiendaServices();
-        
-        ts.agregarProducto(map);
-        ts.modificarPrecio(map);
-        
-        ts.mostrarProductos(map);
-        ts.eliminarProducto(map);
-        
-        ts.mostrarProductos(map);
+
+        int respuesta;
+
+        do {
+
+            System.out.println("========================================");
+            System.out.println("1 - Agregar Producto");
+            System.out.println("2 - Modificar Precio");
+            System.out.println("3 - Eliminar Producto");
+            System.out.println("4 - Mostrar lista productos");
+            System.out.println("5 - Salir");
+            System.out.print("Ingrese una opcion: ");
+            
+            
+            
+            respuesta = entrada.nextInt();
+            respuesta = ts.validarOpcion(respuesta);
+            entrada.nextLine(); // LIMPIA EL BUFFER
+            System.out.println("");
+            
+            switch (respuesta) {
+                case 1:
+                    ts.agregarProducto(map);
+                    System.out.println("Producto/s agregado/s correctamente.");
+                    break;
+                case 2:
+                    ts.modificarPrecio(map);
+                    break;
+                case 3:
+                    ts.eliminarProducto(map);
+                    break;
+                case 4:
+                    ts.mostrarProductos(map);
+                    break;
+                case 5:
+                    System.out.println("SALIENDO DEL PROGRAMA...");
+                    break;
+            }
+           
+            System.out.println("");
+        } while (respuesta != 5);
+
     }
-    
+
 }
